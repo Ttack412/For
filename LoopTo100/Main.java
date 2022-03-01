d10599ba2a368b0d92606975fba8d567e3b88eb4
Ttack412 <31516693+Ttack412@users.noreply.github.com>
2022-02-28 22:38:01 -0500

    Add files via upload

---
+++ LoopTo100/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    // 1. Using a loop print 1-100
+    for(int i = 1; i <= 100; i++)
+      {
+        System.out.println(i);
+      }
+
+  }
+}
