f6b581157bdba7046ea19f7e078bed0ac2cf8e2d
Ttack412 <31516693+Ttack412@users.noreply.github.com>
2022-02-28 22:37:35 -0500

    Add files via upload

---
+++ ForLoop/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    String[] instruments = {"Piano", "Guitar", "Drums", "Keyboard", "Harmonica", "Violin"};
+
+    // 1. Create a for loop that prints out the instruments in the array above. 
+    for(int i = 0; i < instruments.length; i++)
+      {
+        System.out.println(instruments[i]);
+      }
+  
+  }
+}
